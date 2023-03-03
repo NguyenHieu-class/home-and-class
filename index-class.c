b97d8c6d028d77baa53d172500db1ef7e4f051a4
NguyenHieu-class <22010103@st.phenikaa-uni.edu.vn>
2023-03-03 15:20:52 +0700

    class

--- index-class.c
+++ index-class.c
@@ -1,11 +1,95 @@
 #include<stdio.h>
-
+/*
 void main(){
     int a=5;
     float b=1.234;
     printf("Hien thi mot so nguyen a = %f va mot so thuc b = %d\n",a,b);
     
 }
+*/
+
+//void main(){
+    
+    /*
+    printf("%d\n",455);
+    printf("%i\n",455);
+    printf("%d\n",+455);
+    printf("%d\n",-455);
+    printf("%hd\n",32000);
+    printf("%ld\n",200000000000);
+    printf("%o\n",455);
+    printf("%u\n",445);
+    printf("%u\n",-455);
+    printf("%x\n",455);
+    printf("%x\n",455);
+    */
+    /*
+    printf("%e\n",1234567.89);
+    printf("%e\n",+1234567.89);
+    printf("%e\n",-1234567.89);
+    printf("%E\n",1234567.89);
+    printf("%f\n",1234567.89);
+    printf("%g\n",1234567.89);
+    printf("%G\n",1234567.89);
+    
+    */
+    /*
+    int c = 1427;
+    double p = 1427.0;
+    printf("%#o\n",c);
+    printf("%#x\n",c);
+    printf("%#X\n",c);
+    printf("\n%#g\n",p);
+    printf("%#g\n",p);
+    */
+    /*
+    char chr = 'A';
+    char str[] = "This is a string";
+    const char *const_str = "This is also a string";
+    printf("%c\n",chr);
+    printf("%s\n","This is a string");
+    printf("%s\n",str);
+    printf("%s\n",const_str);
+    */
+    /*
+    int *ptr;
+    int x = 12346,y;
+    ptr = &x;
+    printf("The value of ptr is %p\n",ptr);
+    printf("The address of x is %p\n\n",&x);
+    
+    printf("Total chracters printed on this line is: %n",&y);
+    printf("%d\n\n",y);
+    y = printf("This line has 28 characters\n");
+    printf("%d characters were printed\n\n",y);
+    printf("%%");
+    */
+    
+
+//}
+
+/*
+int main(){
+    int i = 873;
+    double f = 123.945;
+    char s[] = "HPBD";
+    printf("Using precision for integer\n");
+    printf("\t%.4d\n\t%.9d\n\n",i,i);
+    printf("floating point");
+    printf("\t%.3f\n\t%.3e\n\t%.3g\n\n",f,f,f);
+    printf("Using string");
+    printf("\t%.11s\n",s);    
+}
+*/
+
+void main(){
+    printf("%10s%10d%10c%10f\n\n","hello",7,'a',1.23);
+    printf("%-10s%-10d%-10c%-10f\n","hello",7,'a',1.23);
+}
+
+
+
+
 
 
 
