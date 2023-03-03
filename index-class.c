fbf6a485df4059d16ea0a12f9b31877e29d725b3
NguyenHieu-class <22010103@st.phenikaa-uni.edu.vn>
2023-03-03 13:48:57 +0700

    class

--- index-class.c
+++ index-class.c
@@ -3,7 +3,7 @@
 void main(){
     int a=5;
     float b=1.234;
-    printf("Hien thi mot so nguyen a = %d va mot so thuc b = %f\n",b,a);
+    printf("Hien thi mot so nguyen a = %f va mot so thuc b = %d\n",a,b);
     
 }
 
